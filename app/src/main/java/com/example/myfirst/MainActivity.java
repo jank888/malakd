e42d4d915010227b91b96070de1c102dbf780537
jank888 <56196662+jank888@users.noreply.github.com>
2019-10-06 13:19:58 +0200

    added list

--- app/src/main/java/com/example/myfirst/MainActivity.java
+++ app/src/main/java/com/example/myfirst/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.myfirst;
 
 import android.os.Bundle;
 import android.view.View;
+import java.util.ArrayList;
+
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,13 +18,22 @@ public class MainActivity extends AppCompatActivity {
     private TextView szovegmezo;
     private Button nyomogomb;
     private EditText bevitel;
+    private ListView listadoboz;
+    private ArrayAdapter<String> adapter;
+
+
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final ArrayList<String> sztringlista = new ArrayList<String>();
+        sztringlista.add("Anonymus");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         bevitel = (EditText) findViewById(R.id.névcella);
         szovegmezo = (TextView) findViewById(R.id.szoveg);
         szovegmezo.setText("Az alkalmazás sikeresen elindult!");
@@ -28,8 +42,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 szovegmezo.setText("Helló " + bevitel.getText() + "!");
+                adapter.add(bevitel.getText().toString());
             }
         });
+
+
+        listadoboz = (ListView) findViewById(R.id.lista);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,sztringlista);
+        listadoboz.setAdapter(adapter);
+        adapter.clear();
+        adapter.add("alap");
+        listadoboz.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String kivalasztott_elem = (String)adapter.getItem(position);
+                szovegmezo.setText("Helló " + kivalasztott_elem + "!");
+            }
+        });
+
 
     }
 }
